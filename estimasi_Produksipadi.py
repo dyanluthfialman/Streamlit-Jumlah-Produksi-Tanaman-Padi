4fa01cdcad5690ccfd6b376fd930e4334613a31e
dyanluthfialman <34535373+dyanluthfialman@users.noreply.github.com>
2023-04-19 02:00:36 +0700

    Add files via upload

---
+++ estimasi_Produksipadi.py
@@ -0,0 +1,22 @@
+import pickle
+import streamlit as st
+
+model = pickle.load(open('Data_Tanaman_Padi_Sumatera_version_1.sav', 'rb'))
+
+st.title('Estimasi')
+st.subheader('Jumlah Produksi Padi Di Pulau Sumatra')
+st.write('---')
+Provinsi,Tahun,Produksi,Luas Panen,Curah hujan,Kelembapan,Suhu rata-rata
+
+Tahun = st.number_input('Input Tahun(Masehi)')
+Luas Panen = st.number_input('Input Luas Panen(m²)')
+Curah hujan = st.number_input('Input Curah hujan (mm)')
+Kelembapan = st.number_input('Input Total Kelembaban(gr)')
+Suhu rata-rata = st.number_input('Input Suhu(°C )')
+
+predict = ''
+
+if st.button('Estimasi Produksi'):
+    predict = model.predict(
+        [[Tahun, Luas Panen, Curah Hujan, Kelembaban, Suhu rata-rata]]    )
+    st.write ('Estimasi jumlah Produksi Padi di Pulau Sumatra (TON) : ', predict)
